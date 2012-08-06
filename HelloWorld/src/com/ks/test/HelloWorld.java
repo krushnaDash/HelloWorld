bf63265250c6c11c7a87c2926e683ab93ec8973b
Krushna <Krushna@J11048>
2012-08-06 10:45:17 +0530

    Testing

--- HelloWorld/src/com/ks/test/HelloWorld.java
+++ HelloWorld/src/com/ks/test/HelloWorld.java
@@ -2,6 +2,7 @@ package com.ks.test;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Happy World");
+		System.out.println("My World");
 	}
 }
