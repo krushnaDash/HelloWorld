cba131c4c0107411e5f5ed8c116969abadb7e67b
Krushna <Krushna@J11048>
2012-08-06 12:59:08 +0530

    A test

--- HelloWorld/src/com/ks/test/HelloWorld.java
+++ HelloWorld/src/com/ks/test/HelloWorld.java
@@ -2,9 +2,7 @@ package com.ks.test;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-
-		System.out.println("Happy World");
-		System.out.println("My World");
-
+		System.out.println("Consumer World");
+		System.out.println("Ford World");
 	}
 }
